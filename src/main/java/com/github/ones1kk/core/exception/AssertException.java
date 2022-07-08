ab2c7483fe193a4fca037c22ca6f47923ebed9c9
ones1kk <ones1k95@gmail.com>
2022-07-09 03:30:56 +0900

    ⚡️ Add : AssertException

---
+++ src/main/java/com/github/ones1kk/core/exception/AssertException.java
@@ -0,0 +1,24 @@
+package com.github.ones1kk.core.exception;
+
+public class AssertException extends RuntimeException {
+
+    public AssertException() {
+        super();
+    }
+
+    public AssertException(String message) {
+        super(message);
+    }
+
+    public AssertException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AssertException(Throwable cause) {
+        super(cause);
+    }
+
+    protected AssertException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
