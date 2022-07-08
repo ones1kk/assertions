fa2d427c2ed50d82c86478131f6eca32245b4aff
ones1kk <ones1k95@gmail.com>
2022-07-08 23:08:38 +0900

    ✅ Add : AbstractAssertTest

---
+++ src/test/java/com/github/ones1kk/core/api/AbstractAssertTest.java
@@ -0,0 +1,27 @@
+package com.github.ones1kk.core.api;
+
+import com.github.ones1kk.core.api.lang.AbstractObjectAssert;
+import com.github.ones1kk.core.api.lang.model.ObjectAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AbstractAssertTest {
+
+    @Test
+    @DisplayName("AbstractObjectAssert Constructor is working well?")
+    public void test1() throws Exception {
+        // given
+        Object actual = new Object();
+
+        // when
+        AbstractObjectAssert<?, Object> abstractObjectAssert = new ObjectAssert(ObjectAssert.class, actual);
+
+        //then
+        assertThat(abstractObjectAssert.actual).isEqualTo(actual);
+        assertThat(abstractObjectAssert.self).isInstanceOf(ObjectAssert.class);
+
+    }
+
+}
