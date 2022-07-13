74bf22f82187416e7774e0155165e7891da5f85c
ones1kk <ones1k95@gmail.com>
2022-07-13 18:30:40 +0900

    ✅ Add : DefaultPrinterTest

---
+++ src/test/java/com/github/ones1kk/asserts/api/feature/print/impl/DefaultPrinterTest.java
@@ -0,0 +1,22 @@
+package com.github.ones1kk.asserts.api.feature.print.impl;
+
+import com.github.ones1kk.asserts.api.feature.print.Printer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultPrinterTest {
+
+    @Test
+    @DisplayName("Check format of output")
+    void test1() {
+        // given
+        Printer printer = new DefaultPrinter();
+
+        // when
+//        printer.writeOutput()
+        // then
+    }
+
+}
