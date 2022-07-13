66223b92be13c040b2f1615da9b09405b00e62d5
ones1kk <ones1k95@gmail.com>
2022-07-13 15:33:43 +0900

    ✨ Add : AssertWrapper

---
+++ src/main/java/com/github/ones1kk/asserts/api/wrapper/impl/CharSequenceAssertWrapper.java
@@ -0,0 +1,35 @@
+package com.github.ones1kk.asserts.api.wrapper.impl;
+
+import com.github.ones1kk.asserts.api.description.Describable;
+import com.github.ones1kk.asserts.api.description.impl.Description;
+import com.github.ones1kk.asserts.api.feature.print.Printer;
+import com.github.ones1kk.asserts.api.feature.print.impl.DefaultPrinter;
+import com.github.ones1kk.asserts.api.wrapper.AssertWrapper;
+import com.github.ones1kk.asserts.api.lang.charsequence.AbstractCharSequenceAssert;
+import com.github.ones1kk.asserts.api.lang.model.CharSequenceAssert;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public class CharSequenceAssertWrapper extends AbstractCharSequenceAssert<CharSequenceAssert, CharSequence> implements AssertWrapper<CharSequenceAssert> {
+
+    protected final Describable describable = new Description();
+
+    protected final Printer printer = new DefaultPrinter();
+
+    public CharSequenceAssertWrapper(CharSequence actual, String asDescription) {
+        super(CharSequenceAssert.class, actual, asDescription);
+    }
+
+    @Override
+    public CharSequenceAssert as(Supplier<String> description, @Nullable Object... args) {
+        String asDescription = describable.as(description, args);
+        return new CharSequenceAssert(actual, printer.writeOutput(actual, asDescription));
+    }
+
+    @Override
+    public CharSequenceAssert as(String description, @Nullable Object... args) {
+        String asDescription = describable.as(description, args);
+        return new CharSequenceAssert(actual, printer.writeOutput(actual, asDescription));
+    }
+}
---
+++ src/main/java/com/github/ones1kk/asserts/api/wrapper/impl/ObjectAssertWrapper.java
@@ -0,0 +1,35 @@
+package com.github.ones1kk.asserts.api.wrapper.impl;
+
+import com.github.ones1kk.asserts.api.description.Describable;
+import com.github.ones1kk.asserts.api.description.impl.Description;
+import com.github.ones1kk.asserts.api.feature.print.Printer;
+import com.github.ones1kk.asserts.api.feature.print.impl.DefaultPrinter;
+import com.github.ones1kk.asserts.api.lang.model.ObjectAssert;
+import com.github.ones1kk.asserts.api.lang.object.AbstractObjectAssert;
+import com.github.ones1kk.asserts.api.wrapper.AssertWrapper;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public class ObjectAssertWrapper extends AbstractObjectAssert<ObjectAssert, Object> implements AssertWrapper<ObjectAssert> {
+
+    protected final Describable describable = new Description();
+
+    protected final Printer printer = new DefaultPrinter();
+
+    public ObjectAssertWrapper(Object actual, String asDescription) {
+        super(ObjectAssert.class, actual, asDescription);
+    }
+
+    @Override
+    public ObjectAssert as(Supplier<String> description, @Nullable Object... args) {
+        String asDescription = describable.as(description, args);
+        return new ObjectAssert(actual, printer.writeOutput(actual, asDescription));
+    }
+
+    @Override
+    public ObjectAssert as(String description, @Nullable Object... args) {
+        String asDescription = describable.as(description, args);
+        return new ObjectAssert(actual, printer.writeOutput(actual, asDescription));
+    }
+}
