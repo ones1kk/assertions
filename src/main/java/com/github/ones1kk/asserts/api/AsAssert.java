53edff8ee7c6aca01b53bf3c1d4a0bb897e9f46e
ones1kk <ones1k95@gmail.com>
2022-07-15 01:22:08 +0900

    ♻️ Refactor : Separate operations

--- src/main/java/com/github/ones1kk/asserts/api/AsAssert.java
+++ src/main/java/com/github/ones1kk/asserts/api/AsAssert.java
@@ -1,6 +1,7 @@
 package com.github.ones1kk.asserts.api;
 
 import com.github.ones1kk.asserts.api.description.Describable;
+import com.github.ones1kk.asserts.api.description.DescriptionHandler;
 import com.github.ones1kk.asserts.api.description.impl.Description;
 import com.github.ones1kk.asserts.api.exception.AssertException;
 import com.github.ones1kk.asserts.api.feature.print.Printer;
@@ -12,15 +13,9 @@ import java.util.function.Supplier;
 
 public class AsAssert<SELF> implements AsAssertInterface<SELF> {
 
-    private final Describable describable = new Description();
-
-    private final Printer printer = new DefaultPrinter();
+    private final DescriptionHandler handler = new DescriptionHandler();
 
     private final SELF self;
-
-    private String description;
-
-    private String asDescription;
 
     protected AsAssert(Class<?> self) {
         this.self = (SELF) self.cast(this);
@@ -28,34 +23,26 @@ public class AsAssert<SELF> implements AsAssertInterface<SELF> {
 
     @Override
     public SELF as(Supplier<String> description, @Nullable Object... args) {
-        this.asDescription = printer.writeOutput(describable.as(description, args));
+        handler.setAsDescription(handler.getPrinter().writeOutput(handler.getDescribable().as(description, args)));
         return self;
     }
 
     @Override
     public SELF as(String description, @Nullable Object... args) {
-        this.asDescription = printer.writeOutput(describable.as(description, args));
+        handler.setAsDescription(handler.getPrinter().writeOutput(handler.getDescribable().as(description, args)));
         return self;
     }
 
     public void setDescription(Object actual, Object expected, String description) {
-        this.description = printer.writeOutput(actual, expected, describable.as(description, actual, expected));
-
+        handler.setDescription(handler.getPrinter().writeOutput(actual, expected, handler.getDescribable().as(description, actual, expected)));
     }
 
     public void setDescription(Object actual, String description) {
-        this.description = printer.writeOutput(actual, describable.as(description, actual));
+        handler.setDescription(handler.getPrinter().writeOutput(actual, handler.getDescribable().as(description, actual)));
     }
 
     public AssertException getException() {
-        if (isEmptyAsDescription()) {
-            return new AssertException(description);
-        }
-        return new AssertException(asDescription);
-    }
-
-    private boolean isEmptyAsDescription() {
-        return StringUtils.isEmpty(asDescription);
+       return handler.getException();
     }
 
 }
---
+++ src/main/java/com/github/ones1kk/asserts/api/description/DescriptionHandler.java
@@ -0,0 +1,44 @@
+package com.github.ones1kk.asserts.api.description;
+
+import com.github.ones1kk.asserts.api.description.impl.Description;
+import com.github.ones1kk.asserts.api.exception.AssertException;
+import com.github.ones1kk.asserts.api.feature.print.Printer;
+import com.github.ones1kk.asserts.api.feature.print.impl.DefaultPrinter;
+import org.apache.commons.lang3.StringUtils;
+
+public class DescriptionHandler {
+
+    private final Printer printer = new DefaultPrinter();
+
+    private final Describable describable = new Description();
+
+    private String description;
+
+    private String asDescription;
+
+    public AssertException getException() {
+        if (isEmptyAsDescription()) {
+            return new AssertException(description);
+        }
+        return new AssertException(asDescription);
+    }
+    private boolean isEmptyAsDescription() {
+        return StringUtils.isEmpty(asDescription);
+    }
+
+    public void setAsDescription(String asDescription) {
+        this.asDescription = asDescription;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Printer getPrinter() {
+        return printer;
+    }
+
+    public Describable getDescribable() {
+        return describable;
+    }
+}
