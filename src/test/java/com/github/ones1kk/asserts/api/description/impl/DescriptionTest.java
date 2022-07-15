ef8e7b2a4c5fa5f376d1f481470638967cfeb1ee
ones1kk <ones1k95@gmail.com>
2022-07-15 17:49:55 +0900

    ✅ Update : DescriptionTest

--- src/test/java/com/github/ones1kk/asserts/api/description/impl/DescriptionTest.java
+++ src/test/java/com/github/ones1kk/asserts/api/description/impl/DescriptionTest.java
@@ -1,15 +1,20 @@
 package com.github.ones1kk.asserts.api.description.impl;
 
+import com.github.ones1kk.asserts.api.Asserts;
 import com.github.ones1kk.asserts.api.description.Describable;
 import com.github.ones1kk.asserts.api.exception.AssertException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DescriptionTest {
 
+    // TODO : Regex
     @Test
     @DisplayName("Description is working well?")
     public void test1() throws Exception {
@@ -19,11 +24,17 @@ class DescriptionTest {
         // when
         String description1 = describable.as("Description test");
         String description2 = describable.as("Description {}", "test");
+        String description3 = "Description test %%s";
+        String description4 = "Description test %s";
+//        Matcher matcher1 = describable.pattern.matcher(description3);
+//        Matcher matcher2 = describable.pattern.matcher(description4);
 
         //then
         assertThat(description1).isEqualTo("Description test");
         assertThat(description2).isEqualTo("Description test");
         assertThrows(AssertException.class, () -> describable.as("Description {}"));
+//        assertThat(matcher1.matches()).isFalse();
+//        assertThat(matcher2.matches()).isTrue();
 
     }
 
