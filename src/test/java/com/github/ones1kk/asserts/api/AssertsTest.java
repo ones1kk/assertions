470609d2d138f32280c3c92ee1d43b3ad48fbe4f
ones1kk <ones1k95@gmail.com>
2022-07-16 17:37:27 +0900

    ✅ Add : AssertsTest

---
+++ src/test/java/com/github/ones1kk/asserts/api/AssertsTest.java
@@ -0,0 +1,27 @@
+package com.github.ones1kk.asserts.api;
+
+import com.github.ones1kk.asserts.api.lang.charsequence.AbstractCharSequenceAssert;
+import com.github.ones1kk.asserts.api.lang.object.AbstractObjectAssert;
+import com.github.ones1kk.asserts.api.lang.string.AbstractStringAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AssertsTest {
+
+    @Test
+    @DisplayName("Return type")
+    public void test1() throws Exception {
+        // given
+        AbstractObjectAssert<?, Object> actual1 = Asserts.that(new Object());
+        AbstractCharSequenceAssert<?, CharSequence> actual2 = Asserts.that((CharSequence) "actual2");
+        AbstractStringAssert<?, String> actual3 = Asserts.that("actual3");
+
+        //then
+        assertThat(actual1).isInstanceOf(AbstractObjectAssert.class);
+        assertThat(actual2).isInstanceOf(AbstractObjectAssert.class);
+        assertThat(actual3).isInstanceOf(AbstractObjectAssert.class);
+    }
+
+}
