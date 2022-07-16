6f1050ceebed64334f219939bcbb6152eee839c7
ones1kk <ones1k95@gmail.com>
2022-07-16 17:37:42 +0900

    ✅ Add : AbstractObjectAssertTest

---
+++ src/test/java/com/github/ones1kk/asserts/api/lang/object/AbstractObjectAssertTest.java
@@ -0,0 +1,43 @@
+package com.github.ones1kk.asserts.api.lang.object;
+
+import com.github.ones1kk.asserts.api.exception.AssertException;
+import com.github.ones1kk.asserts.api.lang.model.ObjectAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class AbstractObjectAssertTest {
+
+    @Test
+    @DisplayName("Object methods test")
+    public void test1() throws Exception {
+        // given
+        Object actual = new Object();
+        AbstractObjectAssert<Object, Object> asset1 = new AbstractObjectAssert<>(ObjectAssert.class, actual);
+        AbstractObjectAssert<Object, Object> assert2 = new AbstractObjectAssert<>(ObjectAssert.class, null);
+
+        //then
+        assert2.isNull();
+        assertThrows(AssertException.class, asset1::isNull);
+
+        asset1.isNotNull();
+        assertThrows(AssertException.class, assert2::isNotNull);
+
+        asset1.isSameAs(actual);
+        assertThrows(AssertException.class, () -> asset1.isSameAs(asset1));
+
+        asset1.isNotSameAs(asset1);
+        assertThrows(AssertException.class, () -> asset1.isNotSameAs(actual));
+
+        asset1.isEqualTo(actual);
+        assertThrows(AssertException.class, () -> asset1.isEqualTo(asset1));
+
+        asset1.isNotEqualTo(asset1);
+        assertThrows(AssertException.class, () -> asset1.isNotEqualTo(actual));
+
+        // Caution : Object can't test about assignableFrom method
+
+    }
+
+}
