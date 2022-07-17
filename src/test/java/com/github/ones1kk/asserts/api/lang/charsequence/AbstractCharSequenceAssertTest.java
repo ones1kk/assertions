748fa4c856d27a3817a75b0d3a22ff8a99ad3b68
ones1kk <ones1k95@gmail.com>
2022-07-18 01:52:08 +0900

    ✅ Add : AbstractCharSequenceAssertTest

---
+++ src/test/java/com/github/ones1kk/asserts/api/lang/charsequence/AbstractCharSequenceAssertTest.java
@@ -0,0 +1,74 @@
+package com.github.ones1kk.asserts.api.lang.charsequence;
+
+import com.github.ones1kk.asserts.api.exception.AssertException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class AbstractCharSequenceAssertTest {
+
+    @Test
+    @DisplayName("Object method test")
+    public void test1() throws Exception {
+        // given
+        CharSequence actual1 = "actual";
+        CharSequence actual2 = null;
+
+        // when
+        AbstractCharSequenceAssert<?, CharSequence> assert1 = new AbstractCharSequenceAssert<>(AbstractCharSequenceAssert.class, actual1);
+        AbstractCharSequenceAssert<?, CharSequence> assert2 = new AbstractCharSequenceAssert<>(AbstractCharSequenceAssert.class, actual2);
+
+        // then
+        assert2.isNull();
+        assertThrows(AssertException.class, assert1::isNull);
+
+        assert1.isNotNull();
+        assertThrows(AssertException.class, assert2::isNotNull);
+
+        assert1.isSameAs(actual1);
+        assertThrows(AssertException.class, () -> assert1.isSameAs(assert1));
+
+        assert1.isNotSameAs(assert1);
+        assertThrows(AssertException.class, () -> assert1.isNotSameAs(actual1));
+
+        assert1.isEqualTo(actual1);
+        assertThrows(AssertException.class, () -> assert1.isEqualTo(assert1));
+
+        assert1.isNotEqualTo(assert1);
+        assertThrows(AssertException.class, () -> assert1.isNotEqualTo(actual1));
+    }
+
+    @Test
+    @DisplayName("CharSequence test")
+    public void test2() throws Exception {
+        // given
+        CharSequence actual1 = "actual";
+        CharSequence actual2 = null;
+        CharSequence actual3 = "";
+        CharSequence actual4 = " ";
+
+        // when
+        AbstractCharSequenceAssert<?, CharSequence> assert1 = new AbstractCharSequenceAssert<>(AbstractCharSequenceAssert.class, actual1);
+        AbstractCharSequenceAssert<?, CharSequence> assert2 = new AbstractCharSequenceAssert<>(AbstractCharSequenceAssert.class, actual2);
+        AbstractCharSequenceAssert<?, CharSequence> assert3 = new AbstractCharSequenceAssert<>(AbstractCharSequenceAssert.class, actual3);
+        AbstractCharSequenceAssert<?, CharSequence> assert4 = new AbstractCharSequenceAssert<>(AbstractCharSequenceAssert.class, actual4);
+
+        // then
+        assert3.isEmpty();
+        assertThrows(AssertException.class, assert1::isEmpty);
+
+        assert1.isNotEmpty();
+        assertThrows(AssertException.class, assert3::isNotEmpty);
+
+        assert4.isBlank();
+        assertThrows(AssertException.class, assert1::isBlank);
+
+        assert1.isNotBlank();
+        assertThrows(AssertException.class, assert4::isNotBlank);
+
+        assert1.isEqualToIgnoreCase("ACTUAL");
+        assertThrows(AssertException.class, () -> assert4.isEqualToIgnoreCase(actual1));
+    }
+
+}
