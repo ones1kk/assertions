20a31a6b1f5ce3341073ef46580d538b35f5e59e
ones1kk <ones1k95@gmail.com>
2022-07-27 00:05:46 +0900

    ✅ Add : BooleansTest

---
+++ src/test/java/com/github/ones1kk/asserts/api/lang/bolean/impl/BooleansTest.java
@@ -0,0 +1,38 @@
+package com.github.ones1kk.asserts.api.lang.bolean.impl;
+
+import com.github.ones1kk.asserts.api.AsAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BooleansTest {
+
+    @Mock
+    AsAssert<?> asAssert;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Test methods")
+    public void test1() throws Exception {
+        // given
+        Booleans booleans = new Booleans(asAssert);
+
+        // when
+        // then
+        assertThrows(Exception.class, () -> booleans.assertIsTrue(false));
+        assertThrows(Exception.class, () -> booleans.assertIsFalse(true));
+
+        booleans.assertIsFalse(false);
+        booleans.assertIsTrue(true);
+
+    }
+
+}
