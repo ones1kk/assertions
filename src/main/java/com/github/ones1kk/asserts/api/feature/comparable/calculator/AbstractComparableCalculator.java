6db44b5ba5043f0054266108ec5ae815e322a656
ones1kk <ones1k95@gmail.com>
2022-07-29 23:53:51 +0900

    ✨ Add : AbstractComparableCalculator

---
+++ src/main/java/com/github/ones1kk/asserts/api/feature/comparable/calculator/AbstractComparableCalculator.java
@@ -0,0 +1,9 @@
+package com.github.ones1kk.asserts.api.feature.comparable.calculator;
+
+public abstract class AbstractComparableCalculator<ACTUAL extends Comparable<ACTUAL>> implements ComparableCalculatorInterface<ACTUAL> {
+
+    protected boolean isPrimitive(ACTUAL actual) {
+        return actual.getClass().isPrimitive();
+    }
+
+}
