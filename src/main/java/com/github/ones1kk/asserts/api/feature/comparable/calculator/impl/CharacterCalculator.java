aee667101394aaa08a5977de0263e2f90aa20d8e
ones1kk <ones1k95@gmail.com>
2022-07-30 00:08:54 +0900

    🐛 Fix : Primitive type compare

--- src/main/java/com/github/ones1kk/asserts/api/feature/comparable/calculator/impl/CharacterCalculator.java
+++ src/main/java/com/github/ones1kk/asserts/api/feature/comparable/calculator/impl/CharacterCalculator.java
@@ -1,26 +1,38 @@
 package com.github.ones1kk.asserts.api.feature.comparable.calculator.impl;
 
-import com.github.ones1kk.asserts.api.feature.comparable.calculator.ComparableCalculatorInterface;
+import com.github.ones1kk.asserts.api.feature.comparable.calculator.AbstractComparableCalculator;
 
-public class CharacterCalculator implements ComparableCalculatorInterface<Character> {
+public class CharacterCalculator extends AbstractComparableCalculator<Character> {
 
     @Override
     public boolean isGraterThan(Character actual, Character expected) {
+        if (isPrimitive(actual, expected)) {
+            return Character.compare(actual, expected) >= 0;
+        }
         return actual.compareTo(expected) >= 0;
     }
 
     @Override
     public boolean isLessThan(Character actual, Character expected) {
+        if (isPrimitive(actual, expected)) {
+            return Character.compare(actual, expected) <= 0;
+        }
         return actual.compareTo(expected) <= 0;
     }
 
     @Override
     public boolean isLessThanOrEqualTo(Character actual, Character expected) {
+        if (isPrimitive(actual, expected)) {
+            return Character.compare(actual, expected) < 0;
+        }
         return actual.compareTo(expected) < 0;
     }
 
     @Override
     public boolean isGraterThanOrEqualTo(Character actual, Character expected) {
+        if (isPrimitive(actual, expected)) {
+            return Character.compare(actual, expected) > 0;
+        }
         return actual.compareTo(expected) > 0;
     }
 
--- src/main/java/com/github/ones1kk/asserts/api/feature/comparable/calculator/impl/DoubleCalculator.java
+++ src/main/java/com/github/ones1kk/asserts/api/feature/comparable/calculator/impl/DoubleCalculator.java
@@ -1,27 +1,39 @@
 package com.github.ones1kk.asserts.api.feature.comparable.calculator.impl;
 
-import com.github.ones1kk.asserts.api.feature.comparable.calculator.ComparableCalculatorInterface;
+import com.github.ones1kk.asserts.api.feature.comparable.calculator.AbstractComparableCalculator;
 
-public class DoubleCalculator implements ComparableCalculatorInterface<Double> {
+public class DoubleCalculator extends AbstractComparableCalculator<Double> {
 
     @Override
     public boolean isGraterThan(Double actual, Double expected) {
-        return Double.compare(actual, expected) > 0;
+        if (isPrimitive(actual, expected)) {
+            return Double.compare(actual, expected) > 0;
+        }
+        return actual.compareTo(expected) > 0;
     }
 
     @Override
     public boolean isLessThan(Double actual, Double expected) {
-        return Double.compare(actual, expected) < 0;
+        if (isPrimitive(actual, expected)) {
+            return Double.compare(actual, expected) < 0;
+        }
+        return actual.compareTo(expected) < 0;
     }
 
     @Override
     public boolean isLessThanOrEqualTo(Double actual, Double expected) {
-        return Double.compare(actual, expected) <= 0;
+        if (isPrimitive(actual, expected)) {
+            return Double.compare(actual, expected) <= 0;
+        }
+        return actual.compareTo(expected) <= 0;
     }
 
     @Override
     public boolean isGraterThanOrEqualTo(Double actual, Double expected) {
-        return Double.compare(actual, expected) >= 0;
+        if (isPrimitive(actual, expected)) {
+            return Double.compare(actual, expected) >= 0;
+        }
+        return actual.compareTo(expected) >= 0;
     }
 
     @Override
