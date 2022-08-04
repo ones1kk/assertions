4f7061bead1996393634c978d3d2dbecdb38117c
ones1kk <ones1k95@gmail.com>
2022-08-04 22:30:02 +0900

    ✏️ Fix : Typos

--- src/main/java/com/github/ones1kk/asserts/core/lang/flot/impl/Floats.java
+++ src/main/java/com/github/ones1kk/asserts/core/lang/flot/impl/Floats.java
@@ -154,8 +154,8 @@ public class Floats extends Objects<Float> implements FloatsInterface<Float> {
     }
 
     private void setAssertClose(Float actual, Float expected, Offset<Float> offset) {
-        // TODO : getter
-        String scope = handler.getDescribable().as("{} is close to {}", actual,
+        
+        String scope = handler.getDescribable().as("{} have to close to {}", actual,
                 (expected - offset.getValue()) + " ~ " + (expected + offset.getValue()));
         handler.setDescription(handler.from(actual, scope));
         throw handler.getException();
