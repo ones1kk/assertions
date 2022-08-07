92a97d843a70d2d4beb63d2c5bee704e365fd1b1
ones1kk <ones1k95@gmail.com>
2022-08-07 19:26:43 +0900

    🚿 Cleanup : Code

--- src/main/java/com/github/ones1kk/asserts/core/description/Describable.java
+++ src/main/java/com/github/ones1kk/asserts/core/description/Describable.java
@@ -2,7 +2,6 @@ package com.github.ones1kk.asserts.core.description;
 
 import javax.annotation.Nullable;
 import java.util.function.Supplier;
-import java.util.regex.Pattern;
 
 public interface Describable {
 
