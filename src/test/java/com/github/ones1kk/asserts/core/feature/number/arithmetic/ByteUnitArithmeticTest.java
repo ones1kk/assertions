9615d916fb6f21cbaa2770fc86be80e2a71d6dd0
ones1kk <ones1k95@gmail.com>
2022-08-07 22:37:03 +0900

    ✅ Add : AritimeticUtilTest

---
+++ src/test/java/com/github/ones1kk/asserts/core/feature/number/arithmetic/ByteUnitArithmeticTest.java
@@ -0,0 +1,38 @@
+package com.github.ones1kk.asserts.core.feature.number.arithmetic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ByteUnitArithmeticTest {
+
+    @Test
+    @DisplayName("Method test")
+    public void test1() throws Exception {
+        // given
+        ByteUnitArithmetic.ArithmeticUtil util = ByteUnitArithmetic.of((byte) 10);
+
+        // when
+        // then
+        assertThat(util.isZero()).isFalse();
+        assertThat(util.isNotZero()).isTrue();
+        assertThat(util.isEven()).isTrue();
+        assertThat(util.isOdd()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Method test(Negative number)")
+    public void test2() throws Exception {
+        // given
+        ByteUnitArithmetic.ArithmeticUtil util = ByteUnitArithmetic.of((byte) -10);
+
+        // when
+        // then
+        assertThat(util.isZero()).isFalse();
+        assertThat(util.isNotZero()).isTrue();
+        assertThat(util.isEven()).isTrue();
+        assertThat(util.isOdd()).isFalse();
+    }
+
+}
---
+++ src/test/java/com/github/ones1kk/asserts/core/feature/number/arithmetic/ShortUnitArithmeticTest.java
@@ -0,0 +1,38 @@
+package com.github.ones1kk.asserts.core.feature.number.arithmetic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShortUnitArithmeticTest {
+
+    @Test
+    @DisplayName("Method test")
+    public void test1() throws Exception {
+        // given
+        ShortUnitArithmetic.ArithmeticUtil util = ShortUnitArithmetic.of((short) 10);
+
+        // when
+        // then
+        assertThat(util.isZero()).isFalse();
+        assertThat(util.isNotZero()).isTrue();
+        assertThat(util.isEven()).isTrue();
+        assertThat(util.isOdd()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Method test(Negative number)")
+    public void test2() throws Exception {
+        // given
+        ShortUnitArithmetic.ArithmeticUtil util = ShortUnitArithmetic.of((short) -10);
+
+        // when
+        // then
+        assertThat(util.isZero()).isFalse();
+        assertThat(util.isNotZero()).isTrue();
+        assertThat(util.isEven()).isTrue();
+        assertThat(util.isOdd()).isFalse();
+    }
+
+}
