4094b67e0488d6dbf63c47f7fe290d2e12fa45cb
ones1kk <ones1k95@gmail.com>
2022-08-09 23:16:28 +0900

    ✨ Add : FileSizeComparable

---
+++ src/main/java/com/github/ones1kk/asserts/core/file/FileSizeComparable.java
@@ -0,0 +1,30 @@
+package com.github.ones1kk.asserts.core.file;
+
+import java.io.File;
+
+public class FileSizeComparable {
+
+    public boolean isLargerThan(File actual, File expected) {
+        return actual.length() > expected.length();
+    }
+
+
+    public boolean isSmallerThan(File actual, File expected) {
+        return actual.length() < expected.length();
+    }
+
+
+    public boolean isSmallerThanOrEqualTo(File actual, File expected) {
+        return actual.length() <= expected.length();
+    }
+
+
+    public boolean isLargerThanOrEqualTo(File actual, File expected) {
+        return actual.length() >= expected.length();
+    }
+
+
+    public boolean is(File actual, File expected) {
+        return actual.equals(expected);
+    }
+}
