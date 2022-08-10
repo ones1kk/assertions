37217a970739b04e112dcbad6cb7f6918b5c0e77
ones1kk <ones1k95@gmail.com>
2022-08-11 00:40:36 +0900

    ✨ Add : ListContainableImpl

--- src/main/java/com/github/ones1kk/asserts/core/feature/iterable/containable/impl/ListContainableImpl.java
+++ src/main/java/com/github/ones1kk/asserts/core/feature/iterable/containable/impl/ListContainableImpl.java
@@ -8,41 +8,54 @@ public class ListContainableImpl<ELEMENT> implements ListContainable<ELEMENT> {
 
     @Override
     public boolean contains(List<? extends ELEMENT> actual, ELEMENT expected) {
-        return false;
+        return actual.contains(expected);
     }
 
     @Override
     public boolean doesNotContain(List<? extends ELEMENT> actual, ELEMENT expected) {
-        return false;
+        return !(contains(actual, expected));
     }
 
     @Override
     public boolean containsAll(List<? extends ELEMENT> actual, ELEMENT... expected) {
-        return false;
+        for (ELEMENT value : expected) {
+            if (!actual.contains(value)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean containsNotAll(List<? extends ELEMENT> actual, ELEMENT... expected) {
-        return false;
+        return !(containsAll(actual, expected));
     }
 
     @Override
     public boolean containsAny(List<? extends ELEMENT> actual, ELEMENT... expected) {
+        for (ELEMENT value : expected) {
+            if (actual.contains(value)) return true;
+        }
         return false;
     }
 
     @Override
     public boolean doseNotContainAny(List<? extends ELEMENT> actual, ELEMENT... expected) {
-        return false;
+        for (ELEMENT value : expected) {
+            if (actual.contains(value)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean containsNull(List<? extends ELEMENT> actual) {
-        return false;
+        return actual.contains(null);
     }
 
     @Override
     public boolean doesNotContainNull(List<? extends ELEMENT> actual) {
-        return false;
+        return !(containsNull(actual));
     }
 }
