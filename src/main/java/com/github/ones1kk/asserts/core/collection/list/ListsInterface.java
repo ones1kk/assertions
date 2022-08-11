0237021d4e5c67b25b779364e86cf4ef992e652d
ones1kk <ones1k95@gmail.com>
2022-08-12 00:38:45 +0900

    ✨ Add : ListAssert > Comparable

--- src/main/java/com/github/ones1kk/asserts/core/collection/list/ListsInterface.java
+++ src/main/java/com/github/ones1kk/asserts/core/collection/list/ListsInterface.java
@@ -1,6 +1,9 @@
 package com.github.ones1kk.asserts.core.collection.list;
 
+import com.github.ones1kk.asserts.core.feature.comparable.Comparable;
 import com.github.ones1kk.asserts.core.feature.iterable.ListIterable;
 
-public interface ListsInterface<ELEMENT> extends ListIterable<ELEMENT> {
+import java.util.List;
+
+public interface ListsInterface<ELEMENT> extends ListIterable<ELEMENT>, Comparable<List<? extends ELEMENT>> {
 }
