ba9b234c53f8979cdf70a94e6975caa070c5ac39
ones1kk <ones1k95@gmail.com>
2022-08-17 23:37:18 +0900

    ✨ Add : MapSupporter

---
+++ src/main/java/com/github/ones1kk/asserts/core/feature/comparable/calculator/collection/impl/MapCalculator.java
@@ -0,0 +1,30 @@
+package com.github.ones1kk.asserts.core.feature.comparable.calculator.collection.impl;
+
+import java.util.Map;
+
+public class MapCalculator<ELEMENT extends Map<K, V>, K, V> {
+
+    public boolean isGraterThan(ELEMENT actual, ELEMENT expected) {
+        return actual.size() > expected.size();
+    }
+
+    public boolean isLessThan(ELEMENT actual, ELEMENT expected) {
+        return actual.size() < expected.size();
+    }
+
+    public boolean isLessThanOrEqualTo(ELEMENT actual, ELEMENT expected) {
+        return actual.size() <= expected.size();
+    }
+
+    public boolean isGraterThanOrEqualTo(ELEMENT actual, ELEMENT expected) {
+        return actual.size() >= expected.size();
+    }
+
+    public boolean hasSameSizeOf(ELEMENT actual, Map<?,?> expected) {
+        return actual.size() == expected.size();
+    }
+
+    public boolean doesNotHaveSameSizeOf(ELEMENT actual, Map<?,?> expected) {
+        return actual.size() != expected.size();
+    }
+}
---
+++ src/main/java/com/github/ones1kk/asserts/core/feature/iterable/containable/impl/MapContainable.java
@@ -0,0 +1,29 @@
+package com.github.ones1kk.asserts.core.feature.iterable.containable.impl;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class MapContainable<ACTUAL extends Map<K, V>, K, V> {
+
+    public boolean containsAllKey(ACTUAL actual, Map<? extends K, ?> expected) {
+        Set<K> actualKeys = actual.keySet();
+        return actualKeys.containsAll(expected.keySet());
+    }
+
+    public boolean doesNotContainAllKey(ACTUAL actual, Map<? extends K, ?> expected) {
+        Set<K> actualKeys = actual.keySet();
+        return !(actualKeys.containsAll(expected.keySet()));
+    }
+
+    public boolean containsAllValue(ACTUAL actual, Map<?, ? extends V> expected) {
+        Collection<V> actualKeys = actual.values();
+        return actualKeys.containsAll(expected.values());
+    }
+
+    public boolean doesNotContainAllValue(ACTUAL actual, Map<?, ? extends V> expected) {
+        Collection<V> actualKeys = actual.values();
+        return !(actualKeys.containsAll(expected.values()));
+    }
+
+}
