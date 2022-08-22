c28282bf6fabff6fdb59a28b0a946f811c39dc7e
ones1kk <ones1k95@gmail.com>
2022-08-23 00:07:23 +0900

    ✨ Add : ComparableTemporalAccessor

---
+++ src/main/java/com/github/ones1kk/asserts/core/feature/comparable/ComparableTemporalAccessor.java
@@ -0,0 +1,19 @@
+package com.github.ones1kk.asserts.core.feature.comparable;
+
+import java.time.temporal.TemporalAccessor;
+
+public interface ComparableTemporalAccessor<ELEMENT extends TemporalAccessor & java.lang.Comparable<ELEMENT>> {
+
+
+    boolean isEqualTo(ELEMENT actual, ELEMENT expected);
+
+    boolean isNotEqualTo(ELEMENT actual, ELEMENT expected);
+
+    boolean isBefore(ELEMENT actual, ELEMENT expected);
+
+    boolean isBeforeOrEqualTo(ELEMENT actual, ELEMENT expected);
+
+    boolean isAfter(ELEMENT actual, ELEMENT expected);
+
+    boolean isAfterOrEqualTo(ELEMENT actual, ELEMENT expected);
+}
---
+++ src/main/java/com/github/ones1kk/asserts/core/feature/comparable/impl/ComparableTemporalAccessorImpl.java
@@ -0,0 +1,38 @@
+package com.github.ones1kk.asserts.core.feature.comparable.impl;
+
+import com.github.ones1kk.asserts.core.feature.comparable.ComparableTemporalAccessor;
+
+import java.time.temporal.TemporalAccessor;
+
+public class ComparableTemporalAccessorImpl<ACTUAL extends TemporalAccessor & Comparable<ACTUAL>> implements ComparableTemporalAccessor<ACTUAL> {
+
+    @Override
+    public boolean isEqualTo(ACTUAL actual, ACTUAL expected) {
+        return actual.compareTo(expected) == 0;
+    }
+
+    @Override
+    public boolean isNotEqualTo(ACTUAL actual, ACTUAL expected) {
+        return  actual.compareTo(expected) != 0;
+    }
+
+    @Override
+    public boolean isBefore(ACTUAL actual, ACTUAL expected) {
+        return actual.compareTo(expected) < 0;
+    }
+
+    @Override
+    public boolean isBeforeOrEqualTo(ACTUAL actual, ACTUAL expected) {
+        return actual.compareTo(expected) <= 0;
+    }
+
+    @Override
+    public boolean isAfter(ACTUAL actual, ACTUAL expected) {
+        return actual.compareTo(expected) > 0;
+    }
+
+    @Override
+    public boolean isAfterOrEqualTo(ACTUAL actual, ACTUAL expected) {
+        return actual.compareTo(expected) >= 0;
+    }
+}
