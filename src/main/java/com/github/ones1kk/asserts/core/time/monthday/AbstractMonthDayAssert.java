e475fa5ad973ae2275de6c9c1e1d5e0b43ad3012
ones1kk <ones1k95@gmail.com>
2022-08-24 00:13:54 +0900

    ✨ ADd : MonthDayAssert

---
+++ src/main/java/com/github/ones1kk/asserts/core/time/monthday/AbstractMonthDayAssert.java
@@ -0,0 +1,12 @@
+package com.github.ones1kk.asserts.core.time.monthday;
+
+import com.github.ones1kk.asserts.core.time.temporal.AbstractTemporalAccessorAssert;
+
+import java.time.MonthDay;
+
+public class AbstractMonthDayAssert<SELF extends AbstractMonthDayAssert<SELF, ACTUAL>, ACTUAL> extends AbstractTemporalAccessorAssert<SELF, MonthDay> {
+
+    public AbstractMonthDayAssert(Class<?> self, MonthDay monthDay) {
+        super(self, monthDay);
+    }
+}
