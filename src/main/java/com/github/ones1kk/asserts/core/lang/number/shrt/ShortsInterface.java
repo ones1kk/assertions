b74a717d82e23f6fffdc99af5a226b19710b0400
ones1kk <ones1k95@gmail.com>
2022-09-01 00:20:03 +0900

    📄 Add : License

--- src/main/java/com/github/ones1kk/asserts/core/lang/number/shrt/ShortsInterface.java
+++ src/main/java/com/github/ones1kk/asserts/core/lang/number/shrt/ShortsInterface.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022. Wonsik Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.ones1kk.asserts.core.lang.number.shrt;
 
 import com.github.ones1kk.asserts.core.feature.comparable.Comparable;
