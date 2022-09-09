7b531c8bee375de2769cb89309ee5cbf42b97997
ones1kk <ones1k95@gmail.com>
2022-09-10 03:30:08 +0900

    ♻️ Refactor : Offset

--- src/main/java/com/github/ones1kk/asserts/core/feature/data/Offset.java
+++ src/main/java/com/github/ones1kk/asserts/core/feature/data/Offset.java
@@ -19,15 +19,23 @@ package com.github.ones1kk.asserts.core.feature.data;
 
 import com.github.ones1kk.asserts.core.Asserts;
 
+/**
+ * <strong> The Offset class receives the offset of the Number class as input. </strong>
+ * <br><br>
+ * <p> Various methods related to offset are provided through verification of whether the input value can be used internally.</p>
+ *
+ * @param <T> inherits {@link Number}
+ */
 public final class Offset<T extends Number> {
 
     private final T value;
 
     private Offset(T value) {
-        Asserts.that(value)
-                .as("Offset value cannot be null")
+        Asserts.that(value.doubleValue())
+                .as("The offset value cannot be null")
                 .isNotNull()
-                .isInstanceOf(Number.class);
+                .as("The offset value should be greater than or equal to zero")
+                .isGreaterThanOrEqualTo(0.0);
         this.value = value;
     }
 
@@ -35,16 +43,44 @@ public final class Offset<T extends Number> {
         return new Offset<>(value);
     }
 
-    public T getValue() {
+    private T getValue() {
         return value;
     }
 
-    public double getBefore(T expected) {
-        return (offset(expected).getValueInternal() - value.doubleValue());
+    /**
+     * Verify {@code expected} is in offset area or not.
+     *
+     * @param actual   reference value.
+     * @param expected expected in offset area.
+     * @return expected is in offset or not.
+     */
+    public boolean isOffset(T actual, T expected) {
+        Asserts.that(expected.doubleValue())
+                .as("expected cannot be null")
+                .isNotNull()
+                .as("Negative numbers cannot be compared")
+                .isPositive();
+        return getBefore(actual) == expected.doubleValue() || getAfter(actual) == expected.doubleValue();
     }
 
-    public double getAfter(T expected) {
-        return (offset(expected).getValueInternal() + value.doubleValue());
+    /**
+     * Get address locations minus a base address.
+     *
+     * @param actual reference value.
+     * @return address locations minus a base address.
+     */
+    public double getBefore(T actual) {
+        return (offset(actual).getValueInternal() - value.doubleValue());
+    }
+
+    /**
+     * Get address locations added to a base address.
+     *
+     * @param actual reference value.
+     * @return address locations added to a base address.
+     */
+    public double getAfter(T actual) {
+        return (offset(actual).getValueInternal() + value.doubleValue());
     }
 
     private double getValueInternal() {
