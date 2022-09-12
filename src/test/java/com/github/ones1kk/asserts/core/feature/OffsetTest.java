d8d5fb76f5373bba6896d6a56f55f6d86d367c42
ones1kk <ones1k95@gmail.com>
2022-09-12 20:15:30 +0900

    ✅ Add : OffsetTest

--- src/test/java/com/github/ones1kk/asserts/core/feature/OffsetTest.java
+++ src/test/java/com/github/ones1kk/asserts/core/feature/OffsetTest.java
@@ -1,10 +1,13 @@
 package com.github.ones1kk.asserts.core.feature;
 
+import com.github.ones1kk.asserts.core.exception.AssertException;
 import com.github.ones1kk.asserts.core.feature.data.Offset;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OffsetTest {
 
@@ -40,5 +43,18 @@ class OffsetTest {
         assertThat((int) offset.getAfter(actual)).isEqualTo(6);
         assertThat(offset.isOffset(actual, expected1)).isTrue();
         assertThat(offset.isOffset(actual, expected2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("Offset method test(failed)")
+    public void test3() throws Exception {
+        // given
+        int value1 = -123;
+        Integer value2 = null;
+
+        // when
+        // then
+        assertThrows(AssertException.class, () -> Offset.offset(value1));
+        assertThrows(AssertException.class, () -> Offset.offset(value2));
     }
 }
