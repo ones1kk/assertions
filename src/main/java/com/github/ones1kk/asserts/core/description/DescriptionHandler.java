79a34c33c7328e774ba3d99cc3e746db570e7f2a
ones1kk <ones1k95@gmail.com>
2022-09-18 02:58:24 +0900

    ♻️ Refactor : Method refactoring(still)

--- src/main/java/com/github/ones1kk/asserts/core/description/DescriptionHandler.java
+++ src/main/java/com/github/ones1kk/asserts/core/description/DescriptionHandler.java
@@ -18,8 +18,8 @@ package com.github.ones1kk.asserts.core.description;
 
 import com.github.ones1kk.asserts.core.description.impl.Description;
 import com.github.ones1kk.asserts.core.exception.AssertException;
-import com.github.ones1kk.asserts.core.feature.print.Printer;
-import com.github.ones1kk.asserts.core.feature.print.impl.DefaultPrinter;
+import com.github.ones1kk.asserts.core.feature.print.PrintFormatter;
+import com.github.ones1kk.asserts.core.feature.print.impl.SimplePrintFormatter;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -27,7 +27,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 public final class DescriptionHandler {
 
-    private final Printer printer = new DefaultPrinter();
+    private final PrintFormatter formatter = new SimplePrintFormatter();
 
     private final Describable describable = new Description();
 
@@ -35,16 +35,28 @@ public final class DescriptionHandler {
 
     private String asDescription;
 
+    public void receive(String description) {
+        setDescription(formatter.writeOutput(description));
+    }
+
+    public void receive(Object actual, String description) {
+        setDescription(formatter.writeOutput(actual, description));
+    }
+
+    public void receive( Object actual, Object expected, String description) {
+         setDescription(formatter.writeOutput(actual, expected, description));
+    }
+
     public String from(String description, Object... args) {
-        return printer.writeOutput(describable.as(description, args));
+        return formatter.writeOutput(describable.as(description, args));
     }
 
     public String from(Object actual, String description) {
-        return printer.writeOutput(actual, describable.as(description, actual));
+        return formatter.writeOutput(actual, describable.as(description, actual));
     }
 
     public String from(Object actual, Object expected, String description) {
-        return printer.writeOutput(actual, expected, describable.as(description, actual, expected));
+        return formatter.writeOutput(actual, expected, describable.as(description, actual, expected));
     }
 
     public AssertException getException() {
