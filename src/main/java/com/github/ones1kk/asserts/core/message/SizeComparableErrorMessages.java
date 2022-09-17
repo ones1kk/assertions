eb868ef276405bae5f5c11686d4040940077fc9d
ones1kk <ones1k95@gmail.com>
2022-09-18 01:14:34 +0900

    ✨ Add : ComparableErrorMessages

---
+++ src/main/java/com/github/ones1kk/asserts/core/message/SizeComparableErrorMessages.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022. Wonsik Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ones1kk.asserts.core.message;
+
+import com.github.ones1kk.asserts.core.description.Describable;
+import com.github.ones1kk.asserts.core.description.impl.Description;
+
+public final class SizeComparableErrorMessages {
+
+    private static final Describable DESCRIBABLE = new Description();
+
+    public static String shouldBeSmallerThan(Object actual, Object expected) {
+        return DESCRIBABLE.as("The given {} should be smaller than {}.", actual, expected);
+    }
+
+    public static String shouldBeSmallerThanOrEqualTo(Object actual, Object expected) {
+        return DESCRIBABLE.as("The given {} should be smaller than or equal to {}.", actual, expected);
+    }
+
+    public static String shouldBeLargerThan(Object actual, Object expected) {
+        return DESCRIBABLE.as("The given {} should be larger than {}.", actual, expected);
+    }
+
+    public static String shouldBeLargerThanOrEqualTo(Object actual, Object expected) {
+        return DESCRIBABLE.as("The given {} should be larger than or equal to {}.", actual, expected);
+    }
+
+    public static String shouldBeBetween(Object actual, Object start, Object end) {
+        return DESCRIBABLE.as("The given size of {} should be between {} and {}.", actual, start, end);
+    }
+}
