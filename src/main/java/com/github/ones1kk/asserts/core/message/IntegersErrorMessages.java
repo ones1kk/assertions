73345702bd2ae90a04e5ff04a7526336a6a8859f
ones1kk <ones1k95@gmail.com>
2022-09-17 20:54:32 +0900

    ✨ Add : NumerableErrorMessages

---
+++ src/main/java/com/github/ones1kk/asserts/core/message/IntegersErrorMessages.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022. Wonsik Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ones1kk.asserts.core.message;
+
+import com.github.ones1kk.asserts.core.description.Describable;
+import com.github.ones1kk.asserts.core.description.impl.Description;
+
+public final class IntegersErrorMessages {
+
+    private static final Describable DESCRIBABLE = new Description();
+
+    private IntegersErrorMessages() {}
+
+    public static String shouldBeOdd(Number actual) {
+        return DESCRIBABLE.as("The given {} should be odd.", actual);
+    }
+
+    public static String shouldNotBeOdd(Number actual) {
+        return DESCRIBABLE.as("The given {} should not be odd.", actual);
+    }
+
+    public static String shouldBeEven(Number actual) {
+        return DESCRIBABLE.as("The given {} should be even.", actual);
+    }
+
+    public static String shouldNotBeEven(Number actual) {
+        return DESCRIBABLE.as("The given {} should not be even.", actual);
+    }
+}
