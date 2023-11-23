3450c02e2c401df3225c3a7c682b2cf56f73decd
ones1kk <ones1k95@gmail.com>
2023-11-23 13:02:02 +0900

    clean up: code

--- src/main/java/io/github/ones1kk/assertion/core/feature/compare/lang/ComparableLanguage.java
+++ src/main/java/io/github/ones1kk/assertion/core/feature/compare/lang/ComparableLanguage.java
@@ -1,7 +1,7 @@
 package io.github.ones1kk.assertion.core.feature.compare.lang;
 
 public interface ComparableLanguage<ACTUAL extends Comparable<ACTUAL>> {
-    
+
     boolean isLessThan(ACTUAL actual, ACTUAL expected);
 
     boolean isLessThanOrEqualTo(ACTUAL actual, ACTUAL expected);
